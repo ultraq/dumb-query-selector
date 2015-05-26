80ed9ca7fe58296fd964fdc122b74a74190c969d
Emanuel Rabina <emanuel.rabina@xero.com>
2015-05-26 15:28:14 +1200

    Create repo for the 'stupid query selector'

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# dumb-query-selector
-A tiny selector function that returns either the matching node or an array of nodes (because `NodeList`s are dumb)
+
+Dumb Query Selector
+===================
+
+A tiny selector function that returns either the matching node or an array of
+nodes (because `NodeList`s are dumb).
---
+++ Source/DumbQuerySelector.js
@@ -0,0 +1,24 @@
+/**
+ * A unified query selector, determining which of the 2 native query selector
+ * methods to use, and then mapping the result set to an array (if the query can
+ * potentially be a list).
+ * 
+ * @param {String} selector
+ * @param {Element} [scope] The scope to limit the search to.  Defaults to
+ *                          document scope.
+ * @return {Array} If `selector` is an ID selector, returns the single node if
+ *                 a match is found, otherwise it returns an array of nodes
+ *                 that match the given selector.
+ * 
+ * @author Emanuel Rabina
+ */
+export default function $(selector, scope = document) {
+
+	// Determine whether we can use the first match or list selector
+	var id = !selector.contains(',') &&
+		((!selector.contains(' ') && selector.startsWith('#')) ||
+		(selector.contains(' ') && selector.substring(selector.lastIndexOf(' ')).contains('#')));
+
+	return id ? document.querySelector(selector) :
+		Array.prototype.slice.call(scope.querySelectorAll(selector));
+}
